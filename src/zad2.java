cf6983d903fc5297eebebbc33ad740b25634a7a4
Mikii2137 <m.igil@wp.pl>
2025-10-15 10:54:44 +0200

    a

--- src/zad2.java
+++ src/zad2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class zadanie2 {
+public class zad2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int wybor;
